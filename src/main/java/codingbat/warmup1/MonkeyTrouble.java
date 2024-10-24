2b2bb013c63328f4d31047bda10f7ff9a45400cf
Ilyas <karimbaev028@gmail.com>
2024-10-24 17:13:29 +0600

    MonkeyTrouble

---
+++ src/main/java/codingbat/warmup1/MonkeyTrouble.java
@@ -0,0 +1,12 @@
+package codingbat.warmup1;
+
+public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if ((aSmile && bSmile) || (!aSmile && !bSmile)){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
